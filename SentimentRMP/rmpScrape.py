b20ca55353bfafa70d54e6ba6b90a590e0a5eee9
d-parkin <82699516+d-parkin@users.noreply.github.com>
2023-09-05 01:05:07 -0700

    Add files via upload
    Initial Commit, csv files created for one professor

---
+++ SentimentRMP/rmpScrape.py
@@ -0,0 +1,56 @@
+import ratemyprofessor
+import datetime
+import csv
+
+professor = ratemyprofessor.get_professor_by_school_and_name(
+    ratemyprofessor.get_school_by_name("California State University Long Beach"), "Jucheol Moon")
+if professor is not None:
+    # Instructor.csv
+    id = 0
+    department = professor.department
+    name = professor.name
+    num_ratings = professor.num_ratings
+
+    headers = ['ID', 'Department', 'Instructor Name', '# Ratings']
+    rows = [id, department, name, num_ratings]
+    with open('Instructor.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+        writer.writerow(rows)
+    file.close()
+
+    # Rating.csv
+    instructor_id = 0
+    ratings = professor.get_ratings()
+    quality, date, difficulty, course, credit, attendance, take_again, grade, review, helpful_pos, helpful_neg = [], [], [], [], [], [], [], [], [], [], []
+    for i in range(len(ratings)):
+        quality.append(ratings[i].rating)
+        formatted_date = ratings[i].date.strftime('%Y-%m-%d %H:%M:%S')
+        date.append(formatted_date)
+        difficulty.append(ratings[i].difficulty)
+        course.append(ratings[i].class_name)
+        credit.append(ratings[i].credit)
+        attendance.append(ratings[i].attendance_mandatory)
+        take_again.append(ratings[i].take_again)
+        grade.append(ratings[i].grade)
+        review.append(ratings[i].comment)
+        helpful_pos.append(ratings[i].thumbs_up)
+        helpful_neg.append(ratings[i].thumbs_down)
+
+
+    headers = ['ID', 'Quality', 'Date', 'Difficulty', 'Course', 'For Credit', 'Attendance Mandatory', 'Would Take Again', 'Grade', 'Review', 'Helpful(Pos)', 'Helpful(Neg)']
+    rows = [instructor_id, quality, date, difficulty, course, credit, attendance, take_again, grade, review, helpful_pos, helpful_neg]
+    with open('Rating.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+        writer.writerow(rows)
+    file.close()
+
+
+
+    
+    # attributes of professor
+    # print(dir(professor))
+    
+   
+
